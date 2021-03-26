3c2ebe044eb5abafe701f8127466db0348377f33
Misi <misi.magenheim@gmail.com>
2021-03-26 13:58:14 +0100

    pff

--- src/main/java/com/codecool/student_scores/StudentScores.java
+++ src/main/java/com/codecool/student_scores/StudentScores.java
@@ -1,5 +1,25 @@
 package com.codecool.student_scores;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.*;
+
 public class StudentScores {
-    
-}
+
+    private final String url;
+    private final String user;
+    private final String password;
+
+    public StudentScores(String url, String user, String password) {
+        this.url = url;
+        this.user = user;
+        this.password = password;
+    }
+
+    private Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(url, user, password);
+    }
+
+
